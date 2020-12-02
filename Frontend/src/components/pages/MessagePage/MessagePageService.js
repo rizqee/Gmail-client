759de97fac7d2f2f20f55efd711c24b1f50f339a
rizqee <arizqee@gmail.com>
2020-12-02 10:29:30 +0700

    bisa enkripsi

--- Frontend/src/components/pages/MessagePage/MessagePageService.js
+++ Frontend/src/components/pages/MessagePage/MessagePageService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 class MessagePageService{
     decryptMessage(data){
-        const URL = "127.0.0.1:3001/traveler/decrypt";
+        const URL = "http://127.0.0.1:3001/traveler/decrypt";
         const payload = data;
         return new Promise((resolve,reject)=>{
             axios.post(URL,payload,{}).then(
